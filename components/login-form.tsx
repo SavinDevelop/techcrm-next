1fa058a60e87d1cbfb45497a42685ab9e80cc0bb
Nikita Savin <savinnikita602@gmail.com>
2025-07-05 14:13:55 +0300

    feat: initialize project with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for various icons and logos in the public directory.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ components/login-form.tsx
@@ -0,0 +1,61 @@
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <form>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <a
+              href="#"
+              className="flex flex-col items-center gap-2 font-medium"
+            >
+              <div className="flex size-8 items-center justify-center rounded-md">
+                <GalleryVerticalEnd className="size-6" />
+              </div>
+              <span className="sr-only">ІОЦ</span>
+            </a>
+            <h1 className="text-xl font-bold">Вітаємо в TechCRM</h1>
+          </div>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="person@dpu.edu.ua"
+                required
+              />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="password"
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+          </div>
+        </div>
+      </form>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  )
+}
